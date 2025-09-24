15119f5751f058266f9d51920164d12e64ab6ef6
sonjongjun <qkskskaktz@gmail.com>
2025-09-24 17:55:43 +0900

    1
    1

---
+++ 1차프로젝트/개인과제모음/js/pizza.js
@@ -0,0 +1,15 @@
+let xicon = document.querySelector('.review-detail .fa-xmark');
+xicon.addEventListener('click', () => {
+    document.querySelector('.review-detail').style.top = '-100%';
+});
+
+
+let detailon = document.querySelector('.review-box');
+detailon.addEventListener('click', () => {
+    document.querySelector('.review-detail').style.top = '0';
+});
+
+let epizza = document.querySelector('.e-pizza');
+epizza.addEventListener('hover', () => {
+    document.
+})
