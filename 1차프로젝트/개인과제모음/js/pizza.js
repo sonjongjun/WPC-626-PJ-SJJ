d70f686c8f46292d81f560fa8f081bcef4e02c7d
sonjongjun <qkskskaktz@gmail.com>
2025-10-10 20:46:49 +0900

    1
    1

--- 1차프로젝트/개인과제모음/js/pizza.js
+++ 1차프로젝트/개인과제모음/js/pizza.js
@@ -37,10 +37,182 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-// .sl-piz8>img에 마우스 오버시
+// .sl-piz8에 마우스 클릭시 section.sl-piz-info 의 translate값을(41% 0%)로 변경
+let piz8 = document.querySelector('.sl-piz8');
+piz8.addEventListener('click', () => {
+    document.querySelector('.sl-piz-info8').style.translate = '41% 0%';
+     document.querySelector('.sl-piz-info2').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info1').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info3').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info4').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info6').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info5').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info7').style.translate = '100% 0%';
+// 트랜지션 효과 부여
+    document.querySelector('.sl-piz-info8').style.transition = 'translate 0.5s ease-in-out';
+});//click이벤트
+// .sl-piz-info8의 x아이콘 클릭시 section.sl-piz-info8 의 translate값을(100% 0%)로 변경
+let xicon9 = document.querySelector('.sl-piz-info8 .fa-xmark');
+xicon9.addEventListener('click', () => {
+    document.querySelector('.sl-piz-info8').style.translate = '100% 0%';
+// 트랜지션 효과 부여
+    document.querySelector('.sl-piz-info8').style.transition = 'translate 0.5s ease-in-out';
+});//click이벤트
+
+// 각각 피자조각에 마우스 클릭시 section.sl-piz-info 의 translate값을(41% 0%)로 변경
+let piz1 = document.querySelector('.sl-piz1');
+let piz2 = document.querySelector('.sl-piz2');
+let piz3 = document.querySelector('.sl-piz3');
+let piz4 = document.querySelector('.sl-piz4');
+let piz5 = document.querySelector('.sl-piz5');
+let piz6 = document.querySelector('.sl-piz6');
+let piz7 = document.querySelector('.sl-piz7');
+piz1.addEventListener('click', () => {
+    document.querySelector('.sl-piz-info1').style.translate = '41% 0%';
+    document.querySelector('.sl-piz-info2').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info3').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info4').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info5').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info6').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info7').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info8').style.translate = '100% 0%';
+// 트랜지션 효과 부여
+    document.querySelector('.sl-piz-info1').style.transition = 'translate 0.5s ease-in-out';
+});//click이벤트
+
+
+piz2.addEventListener('click', () => {
+    document.querySelector('.sl-piz-info2').style.translate = '41% 0%';
+      document.querySelector('.sl-piz-info1').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info3').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info4').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info5').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info6').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info7').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info8').style.translate = '100% 0%';
+// 트랜지션 효과 부여
+    document.querySelector('.sl-piz-info2').style.transition = 'translate 0.5s ease-in-out';
+});//click이벤트
+piz3.addEventListener('click', () => {
+    document.querySelector('.sl-piz-info3').style.translate = '41% 0%';
+    document.querySelector('.sl-piz-info2').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info1').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info4').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info5').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info6').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info7').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info8').style.translate = '100% 0%';
+// 트랜지션 효과 부여
+    document.querySelector('.sl-piz-info3').style.transition = 'translate 0.5s ease-in-out';
+});//click이벤트
+piz4.addEventListener('click', () => {
+    document.querySelector('.sl-piz-info4').style.translate = '41% 0%';
+     document.querySelector('.sl-piz-info2').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info1').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info3').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info5').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info6').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info7').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info8').style.translate = '100% 0%';
+// 트랜지션 효과 부여
+    document.querySelector('.sl-piz-info4').style.transition = 'translate 0.5s ease-in-out';
+});//click이벤트
+piz5.addEventListener('click', () => {
+    document.querySelector('.sl-piz-info5').style.translate = '41% 0%';
+     document.querySelector('.sl-piz-info2').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info1').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info3').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info4').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info6').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info7').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info8').style.translate = '100% 0%';
+// 트랜지션 효과 부여
+    document.querySelector('.sl-piz-info5').style.transition = 'translate 0.5s ease-in-out';
+});//click이벤트
+piz6.addEventListener('click', () => {
+    document.querySelector('.sl-piz-info6').style.translate = '41% 0%';
+     document.querySelector('.sl-piz-info2').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info1').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info3').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info4').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info5').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info7').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info8').style.translate = '100% 0%';
+// 트랜지션 효과 부여
+    document.querySelector('.sl-piz-info6').style.transition = 'translate 0.5s ease-in-out';
+});//click이벤트
+piz7.addEventListener('click', () => {
+    document.querySelector('.sl-piz-info7').style.translate = '41% 0%';
+     document.querySelector('.sl-piz-info2').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info1').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info3').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info4').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info6').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info5').style.translate = '100% 0%';
+    document.querySelector('.sl-piz-info8').style.translate = '100% 0%';
+// 트랜지션 효과 부여
+    document.querySelector('.sl-piz-info7').style.transition = 'translate 0.5s ease-in-out';
+});//click이벤트
+
+
+let xicon2 = document.querySelector('.sl-piz-info1 .fa-xmark');
+xicon2.addEventListener('click', () => {
+    document.querySelector('.sl-piz-info1').style.translate = '100% 0%';
+// 트랜지션 효과 부여
+    document.querySelector('.sl-piz-info1').style.transition = 'translate 0.5s ease-in-out';
+});//click이벤트
+let xicon3 = document.querySelector('.sl-piz-info2 .fa-xmark');
+xicon3.addEventListener('click', () => {
+    document.querySelector('.sl-piz-info2').style.translate = '100% 0%';
+// 트랜지션 효과 부여
+    document.querySelector('.sl-piz-info2').style.transition = 'translate 0.5s ease-in-out';
+});//click이벤트
+let xicon4 = document.querySelector('.sl-piz-info3 .fa-xmark');
+xicon4.addEventListener('click', () => {
+    document.querySelector('.sl-piz-info3').style.translate = '100% 0%';
+// 트랜지션 효과 부여
+    document.querySelector('.sl-piz-info3').style.transition = 'translate 0.5s ease-in-out';
+});//click이벤트
+let xicon5 = document.querySelector('.sl-piz-info4 .fa-xmark');
+xicon5.addEventListener('click', () => {
+    document.querySelector('.sl-piz-info4').style.translate = '100% 0%';
+// 트랜지션 효과 부여
+    document.querySelector('.sl-piz-info4').style.transition = 'translate 0.5s ease-in-out';
+});//click이벤트
+let xicon6 = document.querySelector('.sl-piz-info5 .fa-xmark');
+xicon6.addEventListener('click', () => {
+    document.querySelector('.sl-piz-info5').style.translate = '100% 0%';
+// 트랜지션 효과 부여
+    document.querySelector('.sl-piz-info5').style.transition = 'translate 0.5s ease-in-out';
+});//click이벤트
+let xicon7 = document.querySelector('.sl-piz-info6 .fa-xmark');
+xicon7.addEventListener('click', () => {
+    document.querySelector('.sl-piz-info6').style.translate = '100% 0%';
+// 트랜지션 효과 부여
+    document.querySelector('.sl-piz-info6').style.transition = 'translate 0.5s ease-in-out';
+});//click이벤트
+let xicon8 = document.querySelector('.sl-piz-info7 .fa-xmark');
+xicon8.addEventListener('click', () => {
+    document.querySelector('.sl-piz-info7').style.translate = '100% 0%';
+// 트랜지션 효과 부여
+    document.querySelector('.sl-piz-info7').style.transition = 'translate 0.5s ease-in-out';
+});//click이벤트
 
 
 
 
-//sl-piz-info>i 클릭시 sl-piz-info가 translateX(100%)로 이동
-let
+// sl-piz-info창이 하나씩만 열려있게 하기 (하나가 열리면 나머지는 닫히게 하기)
+
+
+
+  var swiper = new Swiper(".m-banner-box", {
+      loop: true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+
+
+
+
