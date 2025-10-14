fc395c4967b3536678097b1ba9a8ae98c6a9de7d
sonjongjun <qkskskaktz@gmail.com>
2025-10-14 14:18:46 +0900

    1
    1

--- 1차프로젝트/개인과제모음/js/pizza_del.js
+++ 1차프로젝트/개인과제모음/js/pizza_del.js
@@ -16,7 +16,9 @@ document.addEventListener('DOMContentLoaded', function() {
             console.log('버튼 클릭됨, 버튼 내용:', this.innerHTML);
             
             // 모든 black-btn의 opacity를 0으로
-            const allBlackBtns = document.querySelectorAll('.black-btn1, .black-btn2');
+            const allBlackBtns = document.querySelectorAll
+            ('.black-btn1, .black-btn2, .black-btn3, .black-btn4, .black-btn5');
+            
             console.log('찾은 black-btn 개수:', allBlackBtns.length);
             
             allBlackBtns.forEach(black => {
@@ -26,11 +28,19 @@ document.addEventListener('DOMContentLoaded', function() {
             // 클릭된 버튼의 자식 중 black-btn 찾기
             const blackBtn1 = this.querySelector('.black-btn1');
             const blackBtn2 = this.querySelector('.black-btn2');
+            const blackBtn3 = this.querySelector('.black-btn3');
+            const blackBtn4 = this.querySelector('.black-btn4');
+            const blackBtn5 = this.querySelector('.black-btn5');
             
             console.log('black-btn1 찾음:', blackBtn1);
             console.log('black-btn2 찾음:', blackBtn2);
             
             const blackBtn = blackBtn1 || blackBtn2;
+            // blackBtn3,4,5 번은 다중선택이 가능하게 만들기
+            const blackBtn2nd = blackBtn1 || blackBtn2 || blackBtn3 || blackBtn4 || blackBtn5;
+
+            if (blackBtn2nd) {
+                blackBtn.style.opacity = '1';
             
             if (blackBtn) {
                 blackBtn.style.opacity = '1';
