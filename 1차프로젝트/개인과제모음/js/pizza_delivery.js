8bf7ab6a20e05229ca2a2a89791e96c06be8b2b3
sonjongjun <qkskskaktz@gmail.com>
2025-10-24 15:22:36 +0900

    1
    1

--- 1차프로젝트/개인과제모음/js/pizza_delivery.js
+++ 1차프로젝트/개인과제모음/js/pizza_delivery.js
@@ -93,18 +93,18 @@ window.addEventListener('resize', () => {
 });//resize//////////
 
 // button.order-btn 의 ul > li:last-child 클릭시 h2 텍스트를 "총 1개 26,200원 담기"로 바꾸기!
-
-const orderBtn = document.querySelector('.order-btn ul > li:last-child');
+const orderBtn = document.querySelector('.order-btn');
+const orderBtnLast = document.querySelector('.order-btn ul > li:last-child');
 const orderPrice = orderBtn.querySelector('h2');
-orderBtn.addEventListener('click', () => {
+
+orderBtnLast.addEventListener('click', () => {
   orderPrice.innerHTML = '총 1개 26,200원 담기';
-  console.log(orderPrice);
+  // console.log(orderPrice);
 });
 
 // button.order-btn 의 ul > li:first-child 클릭시 h2 텍스트를 "총 1개 38,200원 담기"로 바꾸기!
+const orderBtnFirst = document.querySelector('.order-btn ul > li:first-child');
 
-const orderBtn2 = document.querySelector('.order-btn ul > li:first-child');
-const orderPrice2 = orderBtn2.querySelector('h2');
-orderBtn.addEventListener('click', () => {
-  orderPrice2.innerHTML = '총 1개 38,200원 담기';
+orderBtnFirst.addEventListener('click', () => {
+  orderPrice.innerHTML = '총 1개 38,200원 담기';
 });
