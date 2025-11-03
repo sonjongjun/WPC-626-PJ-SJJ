68a8187016db96d29e54c3084a5f057a8b6922df
sonjongjun <qkskskaktz@gmail.com>
2025-11-03 10:08:05 +0900

    1
    1

--- 2차_팀프로젝트/js/nudake.js
+++ 2차_팀프로젝트/js/nudake.js
@@ -18,7 +18,7 @@ $(window).scroll(function () {
 // li에 hover 되는 동안만
 // 하위에 해당되는 각 p요소들만 opacity 0.5로 1초걸리게 하고 바꾸기!
 $(".pdct-box > ul > li, .colab-box > ul > li, .store-box > ul > li").hover(function () {
-    $(this).children("p").css("opacity", "0.5");
+    $(this).children("p").css("opacity", "0");
 }, function () {
     $(this).children("p").css("opacity", "0");
 });//hover
@@ -204,3 +204,64 @@ $(".pdct > li:nth-child(3)",).click(function () {
   });
 });//click
 
+
+
+/////////////품목 영역3//////////////////////////////
+$(".pdct > li:nth-child(3)",).click(function () {
+  const box = $(".pdct-box3-inbox");
+
+  // 나타나게 (왼쪽에서 오른쪽으로 슬라이드)
+  box.addClass("active");
+  console.log("나 된다");
+
+  // 마우스가 박스 영역을 벗어나면 z-index를 다시 낮춤
+  box.on("mouseleave", function () {
+    box.removeClass("active");
+    // 필요하다면 console.log("마우스 아웃"); 추가 가능
+  });
+});//click
+
+
+// 상단 메뉴 클릭 시 해당 섹션으로 스크롤 이동
+$(".top-menu > ul > li:nth-child(2)").click(function() {
+  // STORES 클릭 시
+  isScrolling = true;
+  const storesSection = $(".store-box").offset().top;
+  $("html, body").animate({
+    scrollTop: storesSection -100
+  }, 800, function() {
+    isScrolling = false;
+  });
+});
+
+$(".top-menu > ul > li:nth-child(3)").click(function() {
+  // PROJECTS(COLLABORATIONS) 클릭 시
+  isScrolling = true;
+  const colabSection = $(".colab-box").offset().top;
+  $("html, body").animate({
+    scrollTop: colabSection - 100
+  }, 800, function() {
+    isScrolling = false;
+  });
+});
+
+$(".top-menu > ul > li:nth-child(4)").click(function() {
+  // MENU(PRODUCTS) 클릭 시
+  isScrolling = true;
+  const pdctSection = $(".pdct-box").offset().top;
+  $("html, body").animate({
+    scrollTop: pdctSection - 100
+  }, 800, function() {
+    isScrolling = false;
+  });
+});
+
+$(".top-menu > ul > li:first-child").click(function() {
+  // NUDAKE 로고 클릭 시 최상단으로
+  isScrolling = true;
+  $("html, body").animate({
+    scrollTop: 0
+  }, 800, function() {
+    isScrolling = false;
+  });
+});
