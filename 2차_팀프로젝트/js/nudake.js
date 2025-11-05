267fde338b13429301e54b3d4724e35aaf3ce7c7
sonjongjun <qkskskaktz@gmail.com>
2025-11-05 18:07:54 +0900

    1
    1

--- 2차_팀프로젝트/js/nudake.js
+++ 2차_팀프로젝트/js/nudake.js
@@ -1,50 +1,51 @@
 //마우스 스크롤을 밑으로 할 때 .header.top-menu가 display none로 1초걸리게 하고 바꾸기
 $(window).scroll(function () {
-    if ($(window).scrollTop() > 0) {
-        $(".top-menu").css("display", "none");
-        
-    }
-})//scroll/////////////
+  if ($(window).scrollTop() > 0) {
+    $(".top-menu").css("display", "none");
+  }
+}); //scroll/////////////
 
 // 스크롤 이벤트 통합 처리
 $(window).scroll(function () {
-    const scrollTop = $(window).scrollTop();
-    const windowHeight = $(window).height();
-    const documentHeight = $(document).height();
-    
-    // 최상단 (0px)
-    if (scrollTop === 0) {
-        $(".top-menu").css("display", "block");
-    }
-    // 최하단 (여유값 10px 추가로 더 잘 감지)
-    else if (scrollTop + windowHeight >= documentHeight - 10) {
-        $(".top-menu").css("display", "block");
-    }
-    // 중간 영역
-    else {
-        $(".top-menu").css("display", "none");
-    }
-});//scroll/////////////
+  const scrollTop = $(window).scrollTop();
+  const windowHeight = $(window).height();
+  const documentHeight = $(document).height();
 
+  // 최상단 (0px)
+  if (scrollTop === 0) {
+    $(".top-menu").css("display", "block");
+  }
+  // 최하단 (여유값 10px 추가로 더 잘 감지)
+  else if (scrollTop + windowHeight >= documentHeight - 10) {
+    $(".top-menu").css("display", "block");
+  }
+  // 중간 영역
+  else {
+    $(".top-menu").css("display", "none");
+  }
+}); //scroll/////////////
 
 // .pdct-box > ul > li, .colab-box > ul > li, .store-box > ul > li:hover시
 // li에 hover 되는 동안만
 // 하위에 해당되는 각 p요소들만 opacity 0.5로 1초걸리게 하고 바꾸기!
-$(".pdct-box > ul > li, .colab-box > ul > li, .store-box > ul > li").hover(function () {
+$(".pdct-box > ul > li, .colab-box > ul > li, .store-box > ul > li").hover(
+  function () {
     $(this).children("p").css("opacity", "0.5");
-}, function () {
+  },
+  function () {
     $(this).children("p").css("opacity", "0");
-});//hover
-
+  }
+); //hover
 
 // html전체에서 부드럽고 일정한 속도로 움직이게 설정하기
 $(window).scroll(function () {
-    if ($(window).scrollTop() > 0) {
-        $("body").css("background-position", "0 " + ($(window).scrollTop() * 0.1) + "px");
-    }
-})
-
-
+  if ($(window).scrollTop() > 0) {
+    $("body").css(
+      "background-position",
+      "0 " + $(window).scrollTop() * 0.1 + "px"
+    );
+  }
+});
 
 //ul.store 밑에 해당되는 첫번째 li클릭시 형제요소인.store-box1-inbox의 z-index 3으로 transition 2s ease-in-out all로 바꾸기
 $(".store > li:first-child").click(function () {
@@ -59,7 +60,7 @@ $(".store > li:first-child").click(function () {
     box.removeClass("active");
     // 필요하다면 console.log("마우스 아웃"); 추가 가능
   });
-});//click
+}); //click
 
 // 도산 매장영역///////////////////////////////////////////////
 $(".store > li:nth-child(2)").click(function () {
@@ -74,8 +75,7 @@ $(".store > li:nth-child(2)").click(function () {
     box.removeClass("active");
     // 필요하다면 console.log("마우스 아웃"); 추가 가능
   });
-});//click
-
+}); //click
 
 // 상하이 매장영역//////////////////////////////////////////////
 $(".store > li:nth-child(3)").click(function () {
@@ -90,11 +90,10 @@ $(".store > li:nth-child(3)").click(function () {
     box.removeClass("active");
     // 필요하다면 console.log("마우스 아웃"); 추가 가능
   });
-});//click
-
+}); //click
 
 /////////////콜라보 영역1//////////////////////////////
-$(".colab > li:first-child",).click(function () {
+$(".colab > li:first-child").click(function () {
   const box = $(".colab-box1-inbox");
 
   // 나타나게 (왼쪽에서 오른쪽으로 슬라이드)
@@ -106,10 +105,10 @@ $(".colab > li:first-child",).click(function () {
     box.removeClass("active");
     // 필요하다면 console.log("마우스 아웃"); 추가 가능
   });
-});//click
+}); //click
 
 /////////////콜라보 영역2//////////////////////////////
-$(".colab > li:nth-child(2)",).click(function () {
+$(".colab > li:nth-child(2)").click(function () {
   const box = $(".colab-box2-inbox");
 
   // 나타나게 (왼쪽에서 오른쪽으로 슬라이드)
@@ -121,10 +120,10 @@ $(".colab > li:nth-child(2)",).click(function () {
     box.removeClass("active");
     // 필요하다면 console.log("마우스 아웃"); 추가 가능
   });
-});//click
+}); //click
 
 /////////////콜라보 영역3//////////////////////////////
-$(".colab > li:nth-child(3)",).click(function () {
+$(".colab > li:nth-child(3)").click(function () {
   const box = $(".colab-box3-inbox");
 
   // 나타나게 (왼쪽에서 오른쪽으로 슬라이드)
@@ -136,10 +135,10 @@ $(".colab > li:nth-child(3)",).click(function () {
     box.removeClass("active");
     // 필요하다면 console.log("마우스 아웃"); 추가 가능
   });
-});//click
+}); //click
 
 /////////////콜라보 영역4//////////////////////////////
-$(".colab > li:nth-child(4)",).click(function () {
+$(".colab > li:nth-child(4)").click(function () {
   const box = $(".colab-box4-inbox");
 
   // 나타나게 (왼쪽에서 오른쪽으로 슬라이드)
@@ -151,10 +150,10 @@ $(".colab > li:nth-child(4)",).click(function () {
     box.removeClass("active");
     // 필요하다면 console.log("마우스 아웃"); 추가 가능
   });
-});//click
+}); //click
 
 /////////////콜라보 영역5//////////////////////////////
-$(".colab > li:nth-child(5)",).click(function () {
+$(".colab > li:nth-child(5)").click(function () {
   const box = $(".colab-box5-inbox");
 
   // 나타나게 (왼쪽에서 오른쪽으로 슬라이드)
@@ -166,12 +165,10 @@ $(".colab > li:nth-child(5)",).click(function () {
     box.removeClass("active");
     // 필요하다면 console.log("마우스 아웃"); 추가 가능
   });
-});//click
-
-
+}); //click
 
 /////////////품목 영역1//////////////////////////////
-$(".pdct > li:first-child",).click(function () {
+$(".pdct > li:first-child").click(function () {
   const box = $(".pdct-box1-inbox");
 
   // 나타나게 (왼쪽에서 오른쪽으로 슬라이드)
@@ -183,11 +180,10 @@ $(".pdct > li:first-child",).click(function () {
     box.removeClass("active");
     // 필요하다면 console.log("마우스 아웃"); 추가 가능
   });
-});//click
-
+}); //click
 
 /////////////품목 영역2//////////////////////////////
-$(".pdct > li:nth-child(2)",).click(function () {
+$(".pdct > li:nth-child(2)").click(function () {
   const box = $(".pdct-box2-inbox");
 
   // 나타나게 (왼쪽에서 오른쪽으로 슬라이드)
@@ -199,11 +195,10 @@ $(".pdct > li:nth-child(2)",).click(function () {
     box.removeClass("active");
     // 필요하다면 console.log("마우스 아웃"); 추가 가능
   });
-});//click
-
+}); //click
 
 /////////////품목 영역3//////////////////////////////
-$(".pdct > li:nth-child(3)",).click(function () {
+$(".pdct > li:nth-child(3)").click(function () {
   const box = $(".pdct-box3-inbox");
 
   // 나타나게 (왼쪽에서 오른쪽으로 슬라이드)
@@ -215,12 +210,10 @@ $(".pdct > li:nth-child(3)",).click(function () {
     box.removeClass("active");
     // 필요하다면 console.log("마우스 아웃"); 추가 가능
   });
-});//click
-
-
+}); //click
 
 /////////////품목 영역3//////////////////////////////
-$(".pdct > li:nth-child(3)",).click(function () {
+$(".pdct > li:nth-child(3)").click(function () {
   const box = $(".pdct-box3-inbox");
 
   // 나타나게 (왼쪽에서 오른쪽으로 슬라이드)
@@ -232,53 +225,67 @@ $(".pdct > li:nth-child(3)",).click(function () {
     box.removeClass("active");
     // 필요하다면 console.log("마우스 아웃"); 추가 가능
   });
-});//click
-
+}); //click
 
 // 상단 메뉴 클릭 시 해당 섹션으로 스크롤 이동
-$(".top-menu > ul > li:nth-child(2)").click(function() {
+$(".top-menu > ul > li:nth-child(2)").click(function () {
   // STORES 클릭 시
   isScrolling = true;
   const storesSection = $(".store-box").offset().top;
-  $("html, body").animate({
-    scrollTop: storesSection -100
-  }, 800, function() {
-    isScrolling = false;
-  });
+  $("html, body").animate(
+    {
+      scrollTop: storesSection - 100,
+    },
+    800,
+    function () {
+      isScrolling = false;
+    }
+  );
 });
 
-$(".top-menu > ul > li:nth-child(3)").click(function() {
+$(".top-menu > ul > li:nth-child(3)").click(function () {
   // PROJECTS(COLLABORATIONS) 클릭 시
   isScrolling = true;
   const colabSection = $(".colab-box").offset().top;
-  $("html, body").animate({
-    scrollTop: colabSection - 100
-  }, 800, function() {
-    isScrolling = false;
-  });
+  $("html, body").animate(
+    {
+      scrollTop: colabSection - 100,
+    },
+    800,
+    function () {
+      isScrolling = false;
+    }
+  );
 });
 
-$(".top-menu > ul > li:nth-child(4)").click(function() {
+$(".top-menu > ul > li:nth-child(4)").click(function () {
   // MENU(PRODUCTS) 클릭 시
   isScrolling = true;
   const pdctSection = $(".pdct-box").offset().top;
-  $("html, body").animate({
-    scrollTop: pdctSection - 100
-  }, 800, function() {
-    isScrolling = false;
-  });
+  $("html, body").animate(
+    {
+      scrollTop: pdctSection - 100,
+    },
+    800,
+    function () {
+      isScrolling = false;
+    }
+  );
 });
 
-$(".top-menu > ul > li:first-child").click(function() {
+$(".top-menu > ul > li:first-child").click(function () {
   // NUDAKE 로고 클릭 시 최상단으로
   isScrolling = true;
-  $("html, body").animate({
-    scrollTop: 0
-  }, 800, function() {
-    isScrolling = false;
-  });
+  $("html, body").animate(
+    {
+      scrollTop: 0,
+    },
+    800,
+    function () {
+      isScrolling = false;
+    }
+  );
 });
-
 
 //cb-arti의 css를 갖고있는 arti-1, arti-2, arti-3, arti-4, arti-5 각자가 클릭될 때
 // translate: 0% 0%로 바꾸기
@@ -302,19 +309,18 @@ $(".cb-arti").click(function () {
     $(this).css("translate", "0% 0%");
   }
   console.log("arti 클릭됨");
-});//click//////////////////////////////////////////////////////////////////
+}); //click//////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////
-
 
 //////////////////////////////////////////////////////////////////////
 // 콜라보 섹션 - 초부드러운 애니메이션
 let isColabLocked = false;
 let colabProgress = 0;
-let colabTargetProgress = 0; // 목표 진행도
+let colabTargetProgress = 0;
 let colabLockPosition = 0;
 let colabCompleted = false;
 const COLAB_ANIMATION_SPEED = 0.012;
-const SMOOTH_FACTOR = 0.15; // 부드러움 정도 (0.1 ~ 0.3)
+const SMOOTH_FACTOR = 0.15;
 
 // easing 함수
 function easeOutQuart(t) {
@@ -350,12 +356,10 @@ function unlockColabScroll() {
 // 부드러운 애니메이션 루프
 function smoothColabAnimation() {
     if (isColabLocked && !colabCompleted) {
-        // 선형 보간으로 부드럽게
         colabProgress += (colabTargetProgress - colabProgress) * SMOOTH_FACTOR;
         
         updateColabAnimation(colabProgress);
         
-        // 완료 체크
         if (colabProgress >= 0.98) {
             colabCompleted = true;
             setTimeout(() => unlockColabScroll(), 200);
@@ -371,7 +375,6 @@ $(window).on('wheel', function(e) {
         e.preventDefault();
         e.stopPropagation();
         
-        // 목표 진행도만 업데이트
         const delta = e.originalEvent.deltaY;
         colabTargetProgress += (delta > 0 ? COLAB_ANIMATION_SPEED : -COLAB_ANIMATION_SPEED);
         colabTargetProgress = Math.max(0, Math.min(1, colabTargetProgress));
@@ -400,7 +403,7 @@ $(window).on('scroll', function() {
             colabProgress = 0;
             colabTargetProgress = 0;
             lockColabScroll(scrollTop);
-            smoothColabAnimation(); // 애니메이션 루프 시작
+            smoothColabAnimation();
         }
         
         if (scrollTop < cbBoxTop - 300 && colabCompleted) {
@@ -429,7 +432,54 @@ function updateColabAnimation(progress) {
     });
 }
 
+// 아티클 클릭 이벤트 (원본 코드 복원)
+$(".cb-arti").each(function () {
+    const initialTranslate = $(this).css("transform");
+    $(this).attr("data-original-translate", initialTranslate);
+});
+
+$(".cb-arti").click(function (e) {
+    e.stopPropagation(); // 이벤트 전파 방지
+    
+    const currentTransform = $(this).css("transform");
+    const isOpen = currentTransform === "matrix(1, 0, 0, 1, 0, 0)" || currentTransform === "none";
+    
+    // 모든 arti를 원래 위치로 복귀
+    $(".cb-arti").each(function () {
+        const original = $(this).attr("data-original-translate");
+        if (original && original !== "none") {
+            $(this).css("transform", original);
+        } else {
+            // data-original-translate가 없으면 클래스에 따라 설정
+            if ($(this).hasClass("arti-1")) {
+                $(this).css("transform", "translateY(90%)");
+            } else if ($(this).hasClass("arti-2")) {
+                $(this).css("transform", "translateY(86%)");
+            } else if ($(this).hasClass("arti-3")) {
+                $(this).css("transform", "translateY(90%)");
+            } else if ($(this).hasClass("arti-4")) {
+                $(this).css("transform", "translateY(87.3%)");
+            } else if ($(this).hasClass("arti-5")) {
+                $(this).css("transform", "translateY(88.5%)");
+            }
+        }
+    });
+    
+    // 클릭한 게 닫힌 상태였다면 — 열기
+    if (!isOpen) {
+        $(this).css("transform", "translateY(0%)");
+    }
+    
+    console.log("arti 클릭됨");
+});
+
 // 초기화
 $(document).ready(function() {
     updateColabAnimation(0);
+    
+    // 아티클 초기 transform 저장
+    $(".cb-arti").each(function() {
+        const initialTransform = $(this).css("transform");
+        $(this).attr("data-original-translate", initialTransform);
+    });
 });
