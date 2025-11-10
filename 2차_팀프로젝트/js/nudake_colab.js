2106b8cf5eb6288be299e846f874a311921fc034
sonjongjun <qkskskaktz@gmail.com>
2025-11-10 11:00:19 +0900

    1
    1

--- 2차_팀프로젝트/js/nudake_colab.js
+++ 2차_팀프로젝트/js/nudake_colab.js
@@ -425,3 +425,15 @@ $(window).scroll(function() {
         }
     }
 });
+
+// 슬라이드 js
+ var swiper = new Swiper(".mySwiper", {
+      effect: "cards",
+      grabCursor: true,
+    });
+
+    //2번쨰 슬라이드 js
+     var swiper2 = new Swiper(".mySwiper2", {
+      effect: "cards",
+      grabCursor: true,
+    }); 
--- 2차_팀프로젝트/js/nudake_store.js
+++ 2차_팀프로젝트/js/nudake_store.js
@@ -36,14 +36,3 @@ $(".top-menu > ul > li:first-child").click(function() {
 });
 
 
-// 슬라이드 js
- var swiper = new Swiper(".mySwiper", {
-      effect: "cards",
-      grabCursor: true,
-    });
-
-    //2번쨰 슬라이드 js
-     var swiper2 = new Swiper(".mySwiper2", {
-      effect: "cards",
-      grabCursor: true,
-    }); 
